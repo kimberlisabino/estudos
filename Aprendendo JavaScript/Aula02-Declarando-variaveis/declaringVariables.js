a88685ee0d66f214099d1ebb458fd44a3301d61a
Kimberli Sabino <kimberli.ariotti@gmail.com>
2025-02-24 05:38:24 -0300

    estudando declaração de variáveis

---
+++ Aprendendo JavaScript/Aula02-Declarando-variaveis/declaringVariables.js
@@ -0,0 +1,25 @@
+const primeiroValor = 10;
+const segundoValor = 20;
+
+console.log("primeiroValor + segundoValor: ", primeiroValor + segundoValor);
+const valorToral = primeiroValor + segundoValor;
+console.log("valorTotal:" , valorToral);
+
+//com a variável "let", o valor pode ser alterado posteriormente
+let terceiroValor = 50;
+console.log("terceiroValor:" , terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValor final:" , terceiroValor);
+
+let comunidade = "Dev Completo";
+//const comunidade = 'Dev Completo';
+//const comunidade = `Dev Completo`;
+
+comunidade = "CDC";
+console.log(`Comunidade: ${comunidade}`);
+
+//padrão camelCase para declarar variáveis
+const alunaDoCurso = "Kimberli";
+//padrão snake_case
+const autora_do_curso = "Kimberli";
