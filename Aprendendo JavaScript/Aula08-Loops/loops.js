66a3bfbe9f751e187bda430d06d506e47a6acc5c
Kimberli Sabino <kimberli.ariotti@gmail.com>
2025-02-27 06:40:23 -0300

    aprendendo sobre loops

---
+++ Aprendendo JavaScript/Aula08-Loops/loops.js
@@ -0,0 +1,22 @@
+// FOR / WHILE / DO WHILE
+
+// FOR
+for (let dia = 1; dia <= 30; dia++)[
+    console.log("Dia", dia)
+]
+
+// WHILE
+let dia = 1;
+while (dia <= 30) {
+    console.log("Dia", dia);
+    dia++;
+}
+
+
+// DO WHILE
+// let dia = 1;
+// do{
+//     console.log("Dia", dia);
+//     dia++; 
+// } while (dia <= 30);
+    
